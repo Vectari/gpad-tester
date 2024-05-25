310c08f00a6db22a76c1000bc337bfc327366f4b
Vectari <mateusz.majer996@gmail.com>
2024-05-25 23:51:08 +0200

    settings and styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import styled from "styled-components";
+
+const StyledH1 = styled.h1`
+  color: red;
+`;
+
 function App() {
-  return <>
-  <h1>Hello</h1>
-  </>;
+  return (
+    <>
+      <StyledH1>Hello</StyledH1>
+    </>
+  );
 }
 
 export default App;
