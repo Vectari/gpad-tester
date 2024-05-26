85a03ed0e39516e5780e240efa1d4292ed98eb78
Vectari <mateusz.majer996@gmail.com>
2024-05-26 23:33:53 +0200

    feat: add Link to 1-4 gpad/player

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,28 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledUl = styled.ul`
+  list-style: none;
+  display: flex;
+  padding: 0;
+  margin: 0;
+
+  li {
+    margin-right: 10px;
+  }
+`;
 
 export function Navbar() {
   return (
     <nav>
-      <ul>
-        <li><Link to="/main">Main</Link></li>
-        <li><Link to="/info">INFO</Link></li>
-      </ul>
+      <StyledUl>
+        <li>
+          <Link to="/main">Main</Link>
+        </li>
+        <li>
+          <Link to="/info">INFO</Link>
+        </li>
+      </StyledUl>
     </nav>
-  )
+  );
 }
