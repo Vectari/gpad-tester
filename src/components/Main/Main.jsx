6c39342106f090784ff0e02c9dc9bb9a469467a8
Vectari <mateusz.majer996@gmail.com>
2024-05-26 23:34:17 +0200

    feat: add Link to 1-4 gpad/player

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,26 +1,35 @@
+import { Link, Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledUl = styled.ul`
+  list-style: none;
+  display: flex;
+  padding: 0;
+  margin: 0;
+
+  li {
+    margin-right: 10px;
+  }
+`;
+
 export function Main() {
   return (
     <>
-      <h1>Main comp</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem incidunt
-        atque veritatis vero modi aliquid distinctio voluptates quas similique
-        dolores, cupiditate natus porro id illum non illo cum itaque dicta.
-        Voluptates vero eos voluptate dolores cum mollitia, laudantium delectus
-        maiores sunt quo fugiat consequatur nesciunt distinctio nulla totam
-        eveniet, qui inventore magnam blanditiis. Doloremque excepturi nulla
-        expedita alias hic nesciunt! Blanditiis quasi facilis necessitatibus
-        corrupti unde? Odio tempora incidunt magni labore ut deserunt alias
-        perferendis animi praesentium aspernatur quaerat ab natus ipsam
-        asperiores consectetur sapiente laboriosam, architecto ea iure ullam!
-        Provident, laborum. Dolores dolorem sed vero vel error adipisci, illo
-        sequi maiores nemo tempore possimus cupiditate placeat odio. Natus animi
-        dolores ad necessitatibus. Voluptate, perferendis delectus commodi
-        necessitatibus veniam nihil? Suscipit, vitae facilis quas a blanditiis
-        ipsam asperiores praesentium optio. Facere dolore ea inventore
-        laboriosam, fuga recusandae error ducimus impedit officia asperiores nam
-        porro sapiente quasi, officiis natus obcaecati dolores.
-      </p>
+      <StyledUl>
+        <li>
+          <Link to="/main/#1">#1</Link>
+        </li>
+        <li>
+          <Link to="/main/#2">#2</Link>
+        </li>
+        <li>
+          <Link to="/main/#3">#3</Link>
+        </li>
+        <li>
+          <Link to="/main/#4">#4</Link>
+        </li>
+      </StyledUl>
+      <Outlet />
     </>
   );
 }
