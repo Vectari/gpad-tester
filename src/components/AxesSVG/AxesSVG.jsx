7a73c2fcf293edbfaf9dd451d7c52bdb82080681
Vectari <mateusz.majer996@gmail.com>
2024-05-30 19:28:41 +0200

    feat: now working on props

--- src/components/AxesSVG/AxesSVG.jsx
+++ src/components/AxesSVG/AxesSVG.jsx
@@ -1,24 +1,11 @@
-"use client";
-
-import { Theme } from "@/app/Theme";
-import { useAtom } from "jotai/react";
-import {
-  atomL3Pressed,
-  atomLeftX,
-  atomLeftY,
-  atomR3Pressed,
-  atomRightX,
-  atomRightY,
-} from "@/GamepadAPI/GamepadAPI";
-
-export function AxesSVG() {
-  const [leftX] = useAtom(atomLeftX);
-  const [leftY] = useAtom(atomLeftY);
-  const [rightX] = useAtom(atomRightX);
-  const [rightY] = useAtom(atomRightY);
-  const [l3Pressed] = useAtom(atomL3Pressed);
-  const [r3Pressed] = useAtom(atomR3Pressed);
-
+export function AxesSVG({
+  leftX,
+  leftY,
+  l3Pressed,
+  rightX,
+  rightY,
+  r3Pressed,
+}) {
   return (
     <>
       <svg height="157" width="335">
@@ -28,34 +15,22 @@ export function AxesSVG() {
             cy="0"
             r="78.5"
             fill="none"
-            stroke={Theme.SVGColors.black}
-            stroke-width={l3Pressed ? "5" : "1"}
+            stroke="black"
+            strokeWidth={l3Pressed ? "5" : "1"}
           ></circle>
-          <line
-            x1="0"
-            y1="-78.5"
-            x2="0"
-            y2="78.5"
-            stroke={Theme.SVGColors.black}
-          ></line>
-          <line
-            x1="-78.5"
-            y1="0"
-            x2="78.5"
-            y2="0"
-            stroke={Theme.SVGColors.black}
-          ></line>
+          <line x1="0" y1="-78.5" x2="0" y2="78.5" stroke="black"></line>
+          <line x1="-78.5" y1="0" x2="78.5" y2="0" stroke="black"></line>
           <line
             x1={leftX * 78.5}
             y1={leftY * 78.5}
-            stroke={Theme.SVGColors.black}
-            stroke-width="1"
+            stroke="black"
+            strokeWidth="1"
           ></line>
           <circle
             cx={leftX * 78.5}
             cy={leftY * 78.5}
             r="4"
-            fill={Theme.SVGColors.black}
+            fill="black"
           ></circle>
         </g>
         <g transform="translate(258.5 78.5) scale(0.95, 0.95)">
@@ -64,34 +39,22 @@ export function AxesSVG() {
             cy="0"
             r="78.5"
             fill="none"
-            stroke={Theme.SVGColors.black}
-            stroke-width={r3Pressed ? "5" : "1"}
+            stroke="black"
+            strokeWidth={r3Pressed ? "5" : "1"}
           ></circle>
-          <line
-            x1="0"
-            y1="-78.5"
-            x2="0"
-            y2="78.5"
-            stroke={Theme.SVGColors.black}
-          ></line>
-          <line
-            x1="-78.5"
-            y1="0"
-            x2="78.5"
-            y2="0"
-            stroke={Theme.SVGColors.black}
-          ></line>
+          <line x1="0" y1="-78.5" x2="0" y2="78.5" stroke="black"></line>
+          <line x1="-78.5" y1="0" x2="78.5" y2="0" stroke="black"></line>
           <line
             x1={rightX * 78.5}
             y1={rightY * 78.5}
-            stroke={Theme.SVGColors.black}
-            stroke-width="1"
+            stroke="black"
+            strokeWidth="1"
           ></line>
           <circle
             cx={rightX * 78.5}
             cy={rightY * 78.5}
             r="4"
-            fill={Theme.SVGColors.black}
+            fill="black"
           ></circle>
         </g>
       </svg>
