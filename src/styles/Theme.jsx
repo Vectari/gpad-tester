9ba4bf4b79f84e9999e8ca24907d1bcf4b378747
Vectari <mateusz.majer996@gmail.com>
2024-06-05 00:25:39 +0200

    feat: add new colors

--- src/styles/Theme.jsx
+++ src/styles/Theme.jsx
@@ -1,4 +1,6 @@
 export const Theme = {
+  connected: "rgb(0, 128, 0)",
+  disconnected: "rgb(199, 35, 35)",
   greyOne: "rgb(214, 214, 214)",
   greyTwo: "rgb(157, 157, 157)",
   greyThree: "rgb(99, 99, 99)",
