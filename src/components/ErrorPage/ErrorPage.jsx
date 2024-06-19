ecd8e2aee0e72f4923d2ab0b6a6648439ef478d2
Vectari <mateusz.majer996@gmail.com>
2024-06-19 23:28:16 +0200

    chore: in case of error user dont see information about it, just refresh page

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -5,11 +5,5 @@ export function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
-  return (
-    <div>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>Refreshing...</p>
-    </div>
-  );
+  return <></>;
 }
