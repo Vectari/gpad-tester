802ddc3540861ccbf9ac4272d54865cda2e67f0c
Vectari <mateusz.majer996@gmail.com>
2024-06-21 01:38:45 +0200

    chore: about update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,6 +17,9 @@ export function About() {
     <>
       <br />
       <h2>GamePad Tester created by Vectari</h2>
+      
+      
+      <br />
       <a href="https://github.com/Vectari/gpad-tester" target="_blank">
         Go to Github!
       </a>
