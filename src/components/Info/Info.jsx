af9eba578bb273ae255080e0fb71ab6bb45e9756
Vectari <mateusz.majer996@gmail.com>
2024-06-22 15:26:22 +0200

    feat: add articles

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -3,7 +3,9 @@ export function Info() {
     <div>
       <h1>Buttons Issues</h1>
       <img src="" alt="img" />
-      <article>article</article>
+      <article>If there are issues with the keys, it is recommended to clean all contacts on the motherboard/flex with alcohol or replace the rubber pads that press the keys.</article>
+      <article>In PS4/PS5, the L2 and R2 rubber bands need to be replaced when they are cut due to use and 100% pressure cannot be achieved.</article>
+      <article></article>
       <h1>LT/RT Issues</h1>
       <img src="" alt="img" />
       <article>article</article>
