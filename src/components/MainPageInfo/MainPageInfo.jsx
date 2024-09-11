ff61994ef3b9e7d47aee837ffa7ec5fd7fa4e5a8
Vectari <mateusz.majer996@gmail.com>
2024-09-11 23:26:19 +0200

    feat: add first article

--- src/components/MainPageInfo/MainPageInfo.jsx
+++ src/components/MainPageInfo/MainPageInfo.jsx
@@ -1,7 +1,25 @@
 export function MainPageInfo() {
   return (
     <>
-      <h1>MainPageInfo</h1>
+      <h1>What is a Web GamePad Tester App and What is it For?</h1>
+      <article>
+        A Web GamePad Tester App is an online tool designed to test and
+        diagnose the functionality of various game controllers like PS4, PS5,
+        Xbox One, and Xbox Series. Whether you’re a gamer troubleshooting issues
+        or a developer fine-tuning controller compatibility, this app offers a
+        fast, accessible way to ensure everything is functioning correctly. By
+        connecting your gamepad to your computer or device, you can view
+        real-time feedback on button presses, joystick movements, trigger
+        sensitivities, and more. This helps identify problems such as sticky
+        buttons, misaligned axes, or non-responsive triggers. The app eliminates
+        the need for specialized software installations, offering users an
+        instant solution for checking gamepad performance from any browser.
+        Additionally, game developers can leverage the app to validate input
+        across different devices, ensuring a seamless gaming experience. Whether
+        you’re diagnosing issues for repair or verifying new hardware setups,
+        this tool simplifies the process of ensuring optimal controller
+        functionality.
+      </article>
     </>
   );
 }
