c83aec55bcd150b7a85ff5a4040a3360cb2e2b69
Vectari <mateusz.majer996@gmail.com>
2025-06-04 23:19:32 +0200

    feat: add prerender

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import prerender from 'vite-plugin-prerender'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    prerender({
+      routes: ['/', '/main/one', '/main/two', '/main/three', '/main/four', '/about'], // podaj wszystkie ścieżki
+    }),
+  ],
 })
