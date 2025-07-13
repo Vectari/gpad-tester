4cbc8f389fdbb1e1a4265a8937851cf6384c4a82
Vectari <mateusz.majer996@gmail.com>
2025-07-13 20:50:20 +0200

    feat: add guides

---
+++ src/components/Guides/guidesData.jsx
@@ -0,0 +1,141 @@
+import { BrokenButtonGuide } from "./articles/Microsoft/Xbox One (original)/BrokenButton";
+import { Nintendo_Switch_teardown } from "./articles/Nintendo/Nintendo Switch/Nintendo_Switch_teardown";
+
+export const guidesData = {
+  Microsoft: {
+    devices: {
+      "Xbox One (original)": {
+        guides: {
+          "How to fix a broken button on Xbox One": <BrokenButtonGuide />,
+        },
+      },
+      "Xbox One S": {
+        guides: {
+          // one: "one",
+        },
+      },
+      "Xbox One X": {
+        guides: {
+          // one: "one",
+        },
+      },
+      "Xbox One Gamepad": {
+        guides: {
+          // one: "one",
+        },
+      },
+      "Xbox Series S": {
+        guides: {
+          // one: "one",
+        },
+      },
+      "Xbox Series X": {
+        guides: {
+          // one: "one",
+        },
+      },
+      "Xbox Elite Series Gamepad": {
+        guides: {
+          // one: "one",
+        },
+      },
+      "Xbox Elite Series 2 Gamepad": {
+        guides: {
+          // one: "one",
+        },
+      },
+    },
+  },
+  Nintendo: {
+    devices: {
+      "Nintendo Switch": {
+        guides: {
+          "Nintendo Switch teardown & disassembly": (
+            <Nintendo_Switch_teardown />
+          ),
+          "Second guide": (
+            <Nintendo_Switch_teardown />
+          )
+        },
+      },
+      "Nintendo Switch Lite": {
+        guides: {
+          // one: "one",
+        },
+      },
+      "Nintendo Switch OLED": {
+        guides: {
+          // one: "one",
+        },
+      },
+      "Nintendo Switch Joy-Con": {
+        guides: {
+          // one: "one",
+        },
+      },
+      "Nintendo Switch 2": {
+        guides: {
+          // one: "one",
+        },
+      },
+      "Nintendo Switch 2 Joy-Con": {
+        guides: {
+          // one: "one",
+        },
+      },
+    },
+  },
+  Sony: {
+    devices: {
+      "PlayStation 3 (PS3)": {
+        guides: {
+          // one: "one",
+        },
+      },
+      "PS3 Gamepad (DualShock 3)": {
+        guides: {
+          // one: "one",
+        },
+      },
+      "PlayStation 4 (PS4)": {
+        guides: {
+          // one: "one",
+        },
+      },
+      "PS4 Gamepad (DualShock 4 V1/V2)": {
+        guides: {
+          // one: "one",
+        },
+      },
+      "PlayStation 5 (PS5)": {
+        guides: {
+          // one: "one",
+        },
+      },
+      "PS5 Gamepad (DualSense)": {
+        guides: {
+          // one: "one",
+        },
+      },
+      "PS5 Gamepad (DualSense Edge)": {
+        guides: {
+          // one: "one",
+        },
+      },
+    },
+  },
+  Valve: {
+    devices: {
+      "SteamDeck 1010 (LCD)": {
+        guides: {
+          // one: "one",
+        },
+      },
+      "SteamDeck 1030 (OLED)": {
+        guides: {
+          // one: "one",
+        },
+      },
+    },
+  },
+};
