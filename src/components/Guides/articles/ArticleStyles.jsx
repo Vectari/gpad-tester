ae7ca94d705035ee54e4d710e20b7f8b07b21b16
Vectari <mateusz.majer996@gmail.com>
2025-07-18 23:25:56 +0200

    style: basic styles

---
+++ src/components/Guides/articles/ArticleStyles.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const StyledArticleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  width: 95%;
+  max-width: 700px;
+
+  h1 {
+    /* color: blue; */
+  }
+
+  li {
+    /* max-width: 100px; */
+    /* padding-left: 0;
+    list-style: decimal; */
+  }
+
+  img {
+    width: 95%;
+  }
+`;
