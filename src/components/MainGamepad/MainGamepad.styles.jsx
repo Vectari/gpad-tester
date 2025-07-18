4909163df5ea9f85ab4cb2b06fa93090d9e6dd71
Vectari <mateusz.majer996@gmail.com>
2025-07-19 01:27:19 +0200

    feat: styles for mobile

--- src/components/MainGamepad/MainGamepad.styles.jsx
+++ src/components/MainGamepad/MainGamepad.styles.jsx
@@ -1,10 +1,23 @@
 import styled from "styled-components";
 import { Theme } from "../../styles/Theme";
 
+export const MobileInfo = styled.p`
+  text-align: center;
+  margin: 1rem;
+  color: ${Theme.disconnected};
+  font-size: 1.3rem;
+  font-weight: bold;
+
+  &::before {
+    content: "The mobile version is limited. For a full test, use the desktop.";
+  }
+  @media (min-width: 769px) {
+    display: none;
+  }
+`;
+
 export const StyledLoader = styled.div`
-  margin: 3rem 0 1rem 0;
-  margin: 3rem auto;
-  margin-bottom: 1rem;
+  margin: 3rem auto 1rem auto;
   color: ${Theme.black};
   box-sizing: border-box;
   display: flex;
@@ -24,6 +37,7 @@ export const StyledLoader = styled.div`
   div:nth-child(2) {
     animation-delay: -0.5s;
   }
+
   @keyframes lds-ripple {
     0% {
       top: 36px;
@@ -54,15 +68,20 @@ export const StyledLoader = styled.div`
       opacity: 0;
     }
   }
+
+  @media (max-width: 765px) {
+    margin: 2rem auto;
+    width: 60px;
+    height: 60px;
+  }
 `;
 
 export const StyledLoaderTextWrapper = styled.span`
-  border: 2px solid black;
-  justify-items: center;
-  width: 1150px;
   border: 3px solid ${Theme.secondary};
   border-radius: 1rem;
   margin-top: 2rem;
+  width: 1150px;
+  justify-items: center;
 
   &.scale08 {
     transform: scale(0.8);
@@ -87,6 +106,13 @@ export const StyledLoaderTextWrapper = styled.span`
     transform: scale(1.2);
     margin-top: 6.5rem;
   }
+
+  @media (max-width: 765px) {
+    width: 90%;
+    font-size: 0.9rem;
+    padding: 5%;
+    text-align: center;
+  }
 `;
 
 export const StyledSVG = styled.div`
@@ -94,7 +120,12 @@ export const StyledSVG = styled.div`
   scale: 1.2;
 
   img {
-    padding: 0 10px 0 10px;
+    padding: 0 10px;
+  }
+
+  @media (max-width: 765px) {
+    scale: 1;
+    margin-bottom: 200px;
   }
 `;
 
@@ -146,6 +177,19 @@ export const StyledContener = styled.div`
   span {
     font-weight: 700;
   }
+
+  @media (max-width: 765px) {
+    grid-template-columns: 1fr;
+    width: 99%;
+    padding: 0.5rem;
+    margin-left: auto;
+    margin-right: auto;
+    
+
+    p {
+      font-size: 1rem;
+    }
+  }
 `;
 
 export const StyledGamepadSVGAxesAVGWrapper = styled.div`
@@ -154,10 +198,16 @@ export const StyledGamepadSVGAxesAVGWrapper = styled.div`
   background-color: ${Theme.white};
   border-radius: 1rem;
   padding: 3rem;
+
+  @media (max-width: 765px) {
+    padding: 0;
+  }
 `;
 
 export const AxesAndButtonsWrapper = styled.div`
-  /* margin-top: 2rem; */
+  @media (max-width: 765px) {
+    max-width: 90%;
+  }
 `;
 
 export const AxesWrapper = styled.div`
@@ -169,6 +219,11 @@ export const AxesWrapper = styled.div`
   background-color: ${Theme.white};
   border-radius: 1rem;
   padding: 1.5rem;
+
+  @media (max-width: 765px) {
+    grid-template-columns: 1fr;
+    margin-top: -1rem;
+  }
 `;
 
 export const ButtonsWrapper = styled.div`
@@ -179,6 +234,10 @@ export const ButtonsWrapper = styled.div`
   background-color: ${Theme.white};
   border-radius: 1rem;
   padding: 2rem;
+
+  @media (max-width: 765px) {
+    grid-template-columns: repeat(5, 1fr);
+  }
 `;
 
 export const StyledButtons = styled.div`
@@ -189,6 +248,11 @@ export const StyledButtons = styled.div`
   border-radius: 10px;
   width: 55px;
   border: 1px solid ${Theme.secondary};
+
+  @media (max-width: 765px) {
+    width: 45px;
+    font-size: 0.75rem;
+  }
 `;
 
 export const HistoryList = styled.ul`
@@ -200,6 +264,10 @@ export const HistoryList = styled.ul`
   margin: 1rem auto;
   background-color: ${Theme.white};
   border-radius: 1rem;
+
+  @media (max-width: 765px) {
+    max-width: 400px;
+  }
 `;
 
 export const HistoryWrapper = styled.div`
@@ -222,8 +290,6 @@ export const HistoryWrapper = styled.div`
     border: 2px solid ${Theme.white};
 
     &:hover {
-      -webkit-box-shadow: 0px 0px 7px 0px rgba(66, 68, 90, 1);
-      -moz-box-shadow: 0px 0px 7px 0px rgba(66, 68, 90, 1);
       box-shadow: 0px 0px 7px 0px rgba(66, 68, 90, 1);
       background-color: ${Theme.white};
       color: ${Theme.secondary};
@@ -244,7 +310,6 @@ export const HistoryWrapper = styled.div`
     }
   }
 
-  /* Custom Scrollbar Styles */
   ::-webkit-scrollbar {
     margin-top: 1rem;
     scrollbar-width: thin;
@@ -257,6 +322,10 @@ export const HistoryWrapper = styled.div`
 
   ::-webkit-scrollbar-thumb:hover {
     background: #555;
+  }
+
+  @media (max-width: 765px) {
+    display: none;
   }
 `;
 
@@ -273,6 +342,10 @@ export const HistoryItem = styled.li`
 export const StyledHistoryAndVibrationButtonsWrapper = styled.div`
   display: flex;
   justify-content: space-between;
+
+  @media (max-width: 765px) {
+    display: none;
+  }
 `;
 
 export const StyledVibrationLoopButtonWrapper = styled.div`
