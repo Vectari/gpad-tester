bb6c13844d9f9fa3edf42e63285a1571b3adfd03
Vectari <mateusz.majer996@gmail.com>
2025-07-24 23:08:00 +0200

    feat: add Article

---
+++ src/components/Guides/articles/Article.jsx
@@ -0,0 +1,31 @@
+import { StyledArticleWrapper } from "../../ArticleStyles";
+
+export function Article() {
+  return (
+    <StyledArticleWrapper>
+      <h1>Note: Some import note here</h1>
+      <h2>● Issue Description</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione
+        nostrum aperiam unde ad nisi culpa, libero itaque veritatis repudiandae
+        nulla.
+      </p>
+      <img src="" alt="" />
+      <h2>● Diagnostics</h2>
+      <ol>
+        <li>Lorem, ipsum dolor.</li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, facere?
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+          labore aspernatur esse similique obcaecati voluptatem. Vero
+          necessitatibus ullam laboriosam sint. Recusandae debitis aperiam
+          ratione possimus nisi rem ipsam quia corporis.
+        </li>
+      </ol>
+      <h2>● Repair Steps</h2>
+      <h2>● Testing</h2>
+    </StyledArticleWrapper>
+  );
+}
