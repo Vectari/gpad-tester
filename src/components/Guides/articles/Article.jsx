9dbe28187243a800297bd7c2a9dc6d40996dbc21
Vectari <mateusz.majer996@gmail.com>
2025-07-24 23:13:49 +0200

    feat: add table

--- src/components/Guides/articles/Article.jsx
+++ src/components/Guides/articles/Article.jsx
@@ -1,6 +1,6 @@
 import { StyledArticleWrapper } from "../../ArticleStyles";
 
-export function Article() {
+export function BrokenButtonGuide() {
   return (
     <StyledArticleWrapper>
       <h1>Note: Some import note here</h1>
@@ -26,6 +26,23 @@ export function Article() {
       </ol>
       <h2>● Repair Steps</h2>
       <h2>● Testing</h2>
+      <table>
+        <tr>
+          <th>One</th>
+          <th>Two</th>
+          <th>Three</th>
+        </tr>
+        <tr>
+          <td>AAA</td>
+          <td>AAA</td>
+          <td>AAA</td>
+        </tr>
+        <tr>
+          <td>BBB</td>
+          <td>BBB</td>
+          <td>BBB</td>
+        </tr>
+      </table>
     </StyledArticleWrapper>
   );
 }
