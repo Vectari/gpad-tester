bf0b2a50341635f7a32cbd59b5142883f0e9d1f9
Vectari <mateusz.majer996@gmail.com>
2025-10-19 12:54:42 +0200

    feat: add mail

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,36 @@
+import styled from "styled-components";
+import { Theme } from "../../styles/Theme";
+
+const StyledSendMail = styled.a`
+  font-size: 1rem;
+  color: ${Theme.white};
+  background-color: ${Theme.interface};
+  padding: 0.8rem;
+  border-radius: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  margin: 1rem auto;
+  max-width: 350px;
+  transition: 0.5s;
+
+  &:hover {
+    border-radius: 1rem;
+    background-color: ${Theme.hover};
+  }
+`;
+
 export function About() {
   return (
     <>
       <br />
       <h2>GamePad Tester created by Vectari</h2>
       <br />
-      <a href="https://github.com/Vectari/gpad-tester" target="_blank">
-        Go to Github!
-      </a>
+      <h3>Any suggestions? Let me know. Thanks!</h3>
+      <StyledSendMail href="mailto:gpadtester.info@gmail.com">
+        Send mail
+      </StyledSendMail>
+      <p>gpadtester.info@gmail.com</p>
       <br />
-      <h3>Any suggestions? Let me know on Github. Thanks!</h3>
     </>
   );
 }
